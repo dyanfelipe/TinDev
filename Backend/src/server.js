d2f893fe51d6843b1ff1c5660062e18c008dbc07
dyanfelipe <dyansilva@outlook.com>
2019-10-05 16:37:53 -0300

    Connect banco de dados

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb+srv://dev:dev@cluster0-o7vg9.mongodb.net/teste?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+
+server.use(express.json())
 server.use(routes);
 
 server.listen(3333);
