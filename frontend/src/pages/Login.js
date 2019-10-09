7dd21d4d4437c57af054b1ddbf2abc77aab27a18
dyanfelipe <dyansilva@outlook.com>
2019-10-08 22:54:20 -0300

    Construindo o front

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../assets/Tindev.svg";
+import "./Login.css"
+
+export default function Login() {
+    return(
+        <div className=" Login-conteiner">
+        <img src={logo} alt="Tindev"></img>
+        </div>
+    );
+}
