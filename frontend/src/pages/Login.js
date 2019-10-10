fda9ed3bc36c398d24a58e7311cbeebfcb3239f7
Dyan Filipe <dyan@henkoti.com.br>
2019-10-10 13:49:41 -0300

    Personalizando logi

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,9 +3,13 @@ import logo from "../assets/Tindev.svg";
 import "./Login.css"
 
 export default function Login() {
-    return(
-        <div className=" Login-conteiner">
-        <img src={logo} alt="Tindev"></img>
+    return (
+        <div className="login-conteiner">
+            <form>
+                <img src={logo} alt="Tindev"></img>
+                <input placeholder="Usuário do gitHub" />
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 }
