08a21a7b5c9d1fd6ecf8080c9b671ecde9c6e48c
dyanfelipe <dyansilva@outlook.com>
2019-10-13 13:27:59 -0300

    alteraçã e integração com a api

---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import logo from "../../assets/Tindev.svg";
+import "./Login.css"
+import api from '../../services/api'
+export default function Login({history}) {
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        const response = await api.post('/devs',{
+            username,
+        })
+        console.log(response);
+        const {_id } = response.data;
+        history.push('/main');
+    }
+    return (
+        <div className="login-conteiner">
+            <form onSubmit={handleSubmit}>
+                <img src={logo} alt="Tindev"></img>
+                <input placeholder="Usuário do gitHub" value = {username} onChange={e => setUsername(e.target.value)}/>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
