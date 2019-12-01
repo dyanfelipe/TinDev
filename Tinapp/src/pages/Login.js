277d7601803880aaafdaad041710ad1475e9bd06
dyanfelipe <dyansilva@outlook.com>
2019-12-01 14:38:18 -0300

    Rota e tratamento.

--- Tinapp/src/pages/Login.js
+++ Tinapp/src/pages/Login.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
-import logo from '../assets/logo.png'
-export default function Login() {
-  return (
-    <View style={styles.container}>
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage'
+import logo from '../assets/logo.png';
+import api from '../services/api';
 
-      <Image source={logo}></Image>
+export default function Login({ navigation }) {
+
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', { user })
+      }
+    })
+  }, []);
+  async function handleLogin() {
+    const response = await api.post('/devs', { username: user });
+
+    const { _id } = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+
+    navigation.navigate('Main', { _id });
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior='padding'
+      enabled={Platform.OS === 'ios'}
+      style={styles.container}>
+      <Image source={logo} />
 
       <TextInput
         autoCapitalize='none'
         autoCorrect={false}
         placeholder='Your Name GitHub'
         placeholderTextColor='#999'
-        style={styles.input} />
-
-      <TouchableOpacity style={styles.button}>
+        style={styles.input}
+        value={user}
+        onChangeText={setUser}
+      />
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
-
-    </View>
+    </KeyboardAvoidingView >
   )
 }
 
